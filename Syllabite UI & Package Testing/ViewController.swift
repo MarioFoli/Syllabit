d20dadcd6265b3b072291c7d01c96d0cacd8ca2c
Anthony Foli <greninjafoli@outlook.com>
2021-12-25 18:42:43 -0600

    Initial Commit

---
+++ Syllabite UI & Package Testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Syllabite UI & Package Testing
+//
+//  Created by Anthony Foli on 12/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
